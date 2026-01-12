b08d34c95318412a0c75598f3fe2086bbfa310ce
Filip <philipmrozik@gmail.com>
2026-01-12 06:13:48 +0100

    adding helper functions

--- tests/rawsql_test.cpp
+++ tests/rawsql_test.cpp
@@ -2,6 +2,32 @@
 #include <string>
 #include <sqlite3.h>
 
+void exec(sqlite3* db, const std::string& sql) {
+    char* err = nullptr;
+    int rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &err);
+
+    if (rc != SQLITE_OK) {
+        std::cerr << "SQL error: " << (err ? err : "") << std::endl;
+        sqlite3_free(err);
+    }
+}
+
+int printCallback(void*, int argc, char** argv, char** colName) {
+    for (int i = 0; i < argc; i++) {
+        std::cout << colName[i] << "="
+                  << (argv[i] ? argv[i] : "NULL");
+        if (i + 1 < argc) std::cout << ", ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
+
+void query(sqlite3* db, const std::string& sql) {
+    char* err = nullptr;
+    sqlite3_exec(db, sql.c_str(), printCallback, nullptr, &err);
+    sqlite3_free(err);
+}
+
 int main() {
     std::cout << "Not ready yet" << std::endl;
 
