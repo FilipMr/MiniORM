794f80ad78cd458a6be7a3459ead1ac656eb06ba
Filip <philipmrozik@gmail.com>
2026-01-13 15:12:49 +0100

    Changing the structure and cleaning

--- src/orm.hpp
+++ src/orm.hpp
@@ -1,6 +1,98 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <type_traits>
+#include <sqlite3.h>
 
-#include "connection.hpp"
-#include "models.hpp"
-#include "mapping.hpp"
-#include "crud.hpp"
+namespace fm {
+
+template <typename T>
+std::string get_sql_type() {
+    if constexpr (std::is_same_v<T, int>) {
+        return "INTEGER";
+    }
+    else if constexpr (std::is_same_v<T, double>) {
+        return "REAL";
+    }
+    else if constexpr (std::is_same_v<T, std::string>) {
+        return "TEXT";
+    }
+    else {
+        return "BLOB";
+    }
+}
+
+// Abstract Column Interface to store diffrent columns
+struct IColumn {
+    virtual std::string get_name() const = 0;
+    virtual std::string get_definition() const = 0;
+    virtual ~IColumn() = default;
+};
+
+class Model {
+protected:
+    std::string table_name;
+    std::vector<IColumn*> columns;
+
+    bool execute_sql(sqlite3* db, const std::string& sql) {
+        char* errMsg = nullptr;
+        int rc = sqlite3_exec(db, sql.c_str(), 0, 0, &errMsg);
+        
+        if (rc != SQLITE_OK) {
+            std::cerr << "SQL Error in " << table_name << ": " << errMsg << std::endl;
+            std::cerr << "Query was: " << sql << std::endl;
+            sqlite3_free(errMsg);
+            return false;
+        }
+        return true;
+    }
+
+public:
+    Model(std::string t_name) : table_name(std::move(t_name)) {
+
+    }
+
+    void register_column(IColumn* col) {
+        columns.push_back(col);
+    }
+
+    bool create_table(sqlite3* db) {
+        std::stringstream ss;
+        ss << "CREATE TABLE IF NOT EXISTS " << table_name << " (";
+        for (size_t i = 0; i < columns.size(); ++i) {
+            ss << columns[i]->get_definition();
+
+            if (i < columns.size() - 1) {
+                ss << ", ";
+            }
+        }
+        ss << ");";
+        
+        return execute_sql(db, ss.str());
+    }
+};
+
+template <typename T>
+class Column : public IColumn {
+private:
+    std::string name;
+    std::string constraints;
+    T value; 
+
+public:
+    Column(Model* owner, std::string col_name, std::string col_constraints = "") : name(std::move(col_name)), constraints(std::move(col_constraints)) {
+        owner->register_column(this);
+    }
+
+    std::string get_name() const override { 
+        return name; 
+    }
+
+    std::string get_definition() const override {
+        return name + " " + get_sql_type<T>() + (constraints.empty() ? "" : " " + constraints);
+    }
+};
+
+}
--- tests/orm_test.cpp
+++ tests/orm_test.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <string>
-#include "src/orm.hpp"
+#include "../src/orm.hpp"
+
+class User : public fm::Model {
+public:
+    fm::Column<int> id{this, "id", "PRIMARY KEY AUTOINCREMENT"};
+    fm::Column<std::string> username{this, "username", "NOT NULL"};
+    fm::Column<int> age{this, "age"};
+
+    User() : fm::Model("users") {}
+};
 
 int main() {
-    std::cout << "Not ready yet" << std::endl;
+    sqlite3* db;
+    sqlite3_open("test.db", &db);
+    
+    User u;
+    u.create_table(db);
+    
+    sqlite3_close(db);
+    return 0;
 
     return 0;
 }
