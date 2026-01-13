b6cc94ff2607e996b28929167625c6e58543a5f0
Filip <philipmrozik@gmail.com>
2026-01-13 16:38:17 +0100

    Add load from. statement for read operation

--- src/IColumn.hpp
+++ src/IColumn.hpp
@@ -10,6 +10,7 @@ public:
     virtual std::string get_name() const = 0;
     virtual std::string get_definition() const = 0;
     virtual void bind_value(sqlite3_stmt* stmt, int index) const = 0;
+    virtual void load_from_stmt(sqlite3_stmt* stmt, int index) = 0;
     virtual ~IColumn() = default;
 };
 
