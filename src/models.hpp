fc4a4fedeb5c0623f131e82c3cb46a16f56bf806
Filip <philipmrozik@gmail.com>
2026-01-13 01:00:40 +0100

    Formatting

--- src/models.hpp
+++ src/models.hpp
@@ -5,16 +5,22 @@
 #include <sstream>
 #include <type_traits>
 
-
-
 namespace fm {
 
 template <typename T>
 std::string get_sql_type() {
-    if constexpr (std::is_same_v<T, int>) return "INTEGER";
-    else if constexpr (std::is_same_v<T, double>) return "REAL";
-    else if constexpr (std::is_same_v<T, std::string>) return "TEXT";
-    else return "BLOB";
+    if constexpr (std::is_same_v<T, int>) {
+        return "INTEGER";
+    }
+    else if constexpr (std::is_same_v<T, double>) {
+        return "REAL";
+    }
+    else if constexpr (std::is_same_v<T, std::string>) {
+        return "TEXT":
+    }
+    else {
+        return "BLOB";
+    }
 }
 
 }
