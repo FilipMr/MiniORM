ce250885db8c16f9c397769fd56b4b4325e10ba8
Filip <philipmrozik@gmail.com>
2026-01-13 19:48:32 +0100

    Refactoring

--- src/IColumn.hpp
+++ src/IColumn.hpp
@@ -7,8 +7,8 @@ namespace fm {
 
 class IColumn {
 public:
-    virtual std::string get_name() const = 0;
-    virtual std::string get_definition() const = 0;
+    virtual std::string getName() const = 0;
+    virtual std::string getDefinition() const = 0;
     virtual void bind_value(sqlite3_stmt* stmt, int index) const = 0;
     virtual void load_from_stmt(sqlite3_stmt* stmt, int index) = 0;
     virtual ~IColumn() = default;
